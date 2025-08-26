0c334ce60baa732e48b5d447689f98cf0594d8c9
adagilis <adagilis@gmail.com>
2025-08-26 06:43:46 -0400

    lec2 edits

--- src/useful_code.jl
+++ src/useful_code.jl
@@ -73,7 +73,28 @@ function geno_mat_to_Int(gm)
     return(geno_mat)
 end
 
-function scaled_loadings(PCA_model)
-    scaled_loadings = loadings(PCA_model).*principalvars(PCA_model).^0.5
+function scaled_loadings(PCA)
+    scaled_loadings = loadings(PCA).*transpose(principalvars(PCA).^0.5)
     return(scaled_loadings)
+end
+
+function broken_stick_dist(r,n)
+    1/n*sum([1/(n-i+1) for i in 1:r])
+end
+
+function broken_stick_pca(pca)
+    prinvars = principalvars(pca)/sum(principalvars(pca))
+    vals = [broken_stick_dist(i,length(prinvars)) for i in 1:length(prinvars)]
+    sig_pcs = findfirst((vals .- prinvars) .< 0)-1
+    return(reverse(vals), sig_pcs)
+end
+
+function scree(pca;broken_stick=false)
+    prinvars = principalvars(pca)/sum(principalvars(pca))
+    ret_plot = bar(prinvars,xlabel="PC",ylabel="Variance explained",legend=false)
+    if broken_stick
+        vals, sig_pcs = broken_stick_pca(pca)
+        scatter!(ret_plot,vals,marker_z=reduce(vcat,[ones(sig_pcs),zeros(length(prinvars)-sig_pcs)]))
+    end
+    return(ret_plot)
 end
